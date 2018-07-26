58095b04f2c8140defe564aa8f055aea8dd32d0c
KomaWasTaken <valentin.gaillard-21@outlook.fr>
2018-07-26 14:44:17 +0200

    ajout fonctions menu, title, form

--- functions.php
+++ functions.php
@@ -1,17 +1,96 @@
 <?php
 define('version', '1.0.0');
-/*** Ajouter un script ou un fichier CSS de la bonne façon */
+
+
+/* Charge le front-end */
 function wpdocs_theme_name_scripts() {
 
+    /* charge les styles */
     wp_enqueue_style('bootstrap-style', get_template_directory_uri()  .'/css/bootstrap.min.css', array() , version , 'all');
     wp_enqueue_style('main-style', get_template_directory_uri()  .'/style.css', array("bootstrap-style") , version , 'all' );
+
+    /* charge les scripts */
     wp_enqueue_script('main-script', get_template_directory_uri()  .'/js/script.js', array('jquery') , version , true );
 
     }
 add_action( 'wp_enqueue_scripts', 'wpdocs_theme_name_scripts' );
 
     
+/* Chargement dans l'admin */
 function admin_scripts(){
-    wp_enqueue_style('bootstrap-adm-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), MACH_VERSION );
+    wp_enqueue_style('bootstrap-adm-core', get_template_directory_uri() . '/css/bootstrap.min.css', array(), version );
 }
 add_action('admin_init', 'admin_scripts');
+
+
+
+function theme_setup(){
+
+    /* support des vignettes */
+    add_theme_support( 'post-thumbnails' );
+
+    /* enlève générateur de versions */
+    remove_action('wp_head', 'wp_generator');
+
+    /* enlève les guillemets à la française */
+    remove_filter ('the_content', 'wptexturize');
+
+    /* support du titre */
+    add_theme_support( 'title-tag' );
+
+    /* support du menu */
+    register_nav_menus( array( 
+		'primary' => _x( 'Primary', 'nav menu location', 'toivo-lite' ),
+		'top'     => _x( 'Top', 'nav menu location', 'toivo-lite' ),
+		'social'  => _x( 'Social', 'nav menu location', 'toivo-lite' )
+    ) );
+    
+    add_theme_support( 'html5', array(
+		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+	) );
+}
+
+add_action('after_setup_theme', 'theme_setup');
+
+/**
+ * Display descriptions in main navigation.
+ *
+ * @param  string  $item_output The menu item output.
+ * @param  WP_Post $item        Menu item object.
+ * @param  int     $depth       Depth of the menu.
+ * @param  array   $args        wp_nav_menu() arguments.
+ * @return string  Menu item with possible description.
+ * @since  1.0.0
+ */
+function toivo_lite_nav_description( $item_output, $item, $depth, $args ) {
+	if ( 'primary' == $args->theme_location && $item->description ) {
+		$extra_class = ( 0 === $depth ) ? ' top-depth' : '';
+		$item_output = str_replace( $args->link_after . '</a>', '<span class="menu-item-description' . $extra_class . '"><span class="menu-item-description-mark">' . ' ' . _x( '&ndash;' , 'dash before menu item description', 'toivo-lite' ) . '</span> ' . $item->description . '</span>' . $args->link_after . '</a>', $item_output );
+	}
+
+	return $item_output;
+}
+add_filter( 'walker_nav_menu_start_el', 'toivo_lite_nav_description', 10, 4 );
+
+/**
+ * Add body classes.
+ *
+ * @param  array  $classes  body classes.
+ * @return array  $classes  body classes.
+ * @since  1.0.0
+ */
+function toivo_lite_extra_layout_classes( $classes ) {
+	
+	/* Add the '.custom-header-image' class if the user is using a custom header image. */
+	if ( get_header_image() ) {
+		$classes[] = 'custom-header-image';
+	}
+	
+	/* Add the 'top-menus-disabled' class if the user is not using top and social menu. */
+	if ( !has_nav_menu( 'top' ) && !has_nav_menu( 'social' ) ) {
+		$classes[] = 'top-menus-disabled';
+    }
+    return $classes;
+	
+}
+add_filter( 'body_class', 'toivo_lite_extra_layout_classes' );
--- index.php
+++ index.php
@@ -1,49 +1,46 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<meta name="author" content="">
-<title>Exemple de thème WordPress</title>
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Romain.W && Valentin.G">
+    <title>Exemple de thème WordPress</title>
+    <?php wp_head(); ?>
 </head>
 <body>
-<div class="header">
-<div class="container">
-<nav id="navigation-principale" role="navigation">
-<a class="active" href="#">Accueil</a>
-<a href="#">Services</a>
-<a href="#">À Propos</a>
-<a href="#">Contact</a>
-</nav>
-</div>
-</div>
-<div class="container">
-<div class="blog-header">
-<h1 class="blog-title">Exemple de thème WordPress</h1>
-<p class="lead blog-description">Création d’un thème WordPress à titre de tutoriel sur WP Pour Les Nuls.</p>
-</div>
-<div class="row">
-<div class="col-sm-8 blog-main">
-<div class="blog-post">
-<h2 class="blog-post-title">Un article de blog</h2>
-<p class="blog-post-meta">20 décembre 2017 par <a href="#">Louis-Philippe</a></p>
-<p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-</div>
-</div>
-<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-<div class="sidebar-module sidebar-module-inset">
-<h4>À Propos</h4>
-<p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-</div>
-</div>
-</div>
-</div>
-<footer class="blog-footer">
-<?php wp_footer(); ?>
-</footer>
+
+
+    <div class="header">
+        <div class="container">
+            <?php wp_nav_menu(); ?>
+        </div>
+    </div>
+    <div class="container">
+        <div class="blog-header">
+            <h1 class="blog-title">Exemple de thème WordPress</h1>
+            <p class="lead blog-description">Création d’un thème WordPress à titre de tutoriel sur WP Pour Les Nuls.</p>
+        </div>
+        <div class="row">
+            <div class="col-sm-8 blog-main">
+                <div class="blog-post">
+                    <h2 class="blog-post-title">Un article de blog</h2>
+                    <p class="blog-post-meta">20 décembre 2017 par <a href="#">Louis-Philippe</a></p>
+                    <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+                </div>
+            </div>
+            <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+                <div class="sidebar-module sidebar-module-inset">
+                    <h4>À Propos</h4>
+                    <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="blog-footer">
+        <?php wp_footer(); ?>
+    </footer>
 
 
 </body>
